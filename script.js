8d472492a9b9e94dc68759cffa7d5ef6c8982736
Arslonov Abdulloh <lwcardinal12@gmail.com>
2025-09-09 21:11:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 // Kelajakdagi interaktivlik uchun joy
 console.log("Koreys tili sayti ishga tushdi!");
+
+// Service Worker'ni ro'yxatdan o'tkazish
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service Worker muvaffaqiyatli ro\'yxatdan o\'tdi!');
+      })
+      .catch(error => {
+        console.log('Service Worker ro\'yxatdan o\'tishda xatolik:', error);
+      });
+  });
+}
